ffea2225ccc301e3efc814b53387e39e9ea65efe
Vyacheslav <v.guber2000@gmail.com>
2022-06-25 12:08:04 +0300

    Filling data

--- app/src/main/java/com/vgubert/loockjson/data/room/MoviesRoomDatabase.kt
+++ app/src/main/java/com/vgubert/loockjson/data/room/MoviesRoomDatabase.kt
@@ -1,4 +1,30 @@
 package com.vgubert.loockjson.data.room
 
-abstract class MoviesRoomDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.vgubert.loockjson.data.room.dao.MoviesDao
+import com.vgubert.loockjson.models.MovieItemModel
+
+@Database(entities = [MovieItemModel::class], version = 1)
+abstract class MoviesRoomDatabase: RoomDatabase() {
+
+    abstract fun getMovieDao(): MoviesDao
+
+    companion object {
+        private var database: MoviesRoomDatabase ?= null
+
+        fun getInstance(context: Context): MoviesRoomDatabase {
+            return if (database == null) {
+                database = Room
+                    .databaseBuilder(context, MoviesRoomDatabase::class.java, "db")
+                    .build()
+                database as MoviesRoomDatabase
+            } else {
+                database as MoviesRoomDatabase
+            }
+        }
+
+    }
 }
